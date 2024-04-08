3a70fa6813de46e416baf4749915d619f42670d3
Bhargava-123 <bhargavashankar2003@gmail.com>
2024-04-08 23:05:42 +0530

    FEAT: changed to dark mode, better hero

--- portfolio-frontend/src/components/Hero.jsx
+++ portfolio-frontend/src/components/Hero.jsx
@@ -5,7 +5,10 @@ const Hero = () => {
   return (
     <div className='hero-container'>
           <div className="hero-content">
-              Developer.
+        <p>
+          👋I'm Bhargava, a seasoned backend web developer 👨‍💻 specializing in various frameworks. My portfolio showcases a diverse range of projects, from web applications to APIs, highlighting my ability to deliver efficient, scalable solutions. With a keen eye for detail and a commitment to continuous learning.
+
+      </p>
           </div>
           <div className="hero-image">
               <img src={heroLogo} alt="" />
--- portfolio-frontend/src/components/Navbar.jsx
+++ portfolio-frontend/src/components/Navbar.jsx
@@ -2,16 +2,22 @@ import React from 'react'
 import "./css/Navbar.scss";
 const Navbar = () => {
   return (
-        <div className="navbar-container">
-              <div className="navbar-link">
-                  LinkedIn
-              </div>
-              <div className="navbar-link">
-                  Github
-              </div>
-              <div className="navbar-link">
-                  Instagram
-          </div>
+    <div className="navbar-container">
+        <div className="logo-name">
+        Bhargava Shankar.
+      </div>
+      <div className="link-container">
+        <div className="navbar-link">
+          LinkedIn
+        </div>
+        <div className="navbar-link">
+          Github
+        </div>
+        <div className="navbar-link">
+          Instagram
+        </div>
+      </div>
+              
         </div>
   )
 }
